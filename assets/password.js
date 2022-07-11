08d673b84570a9c37617d8c7d0f534fd66fb57b8
Keanu Rivers <madlad@Keanus-MacBook-Pro.local>
2022-07-10 21:29:14 -0700

    Added variables and confirmations, tested working

--- assets/password.js
+++ assets/password.js
@@ -1,12 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lletters = "abcdefghijklmnopqrstuvwxyz";
+var scharacter = "#$%^&!"
+var nmbrs = "1234567890"
+var validCharacters = ""
+var validPassword = ""
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordlength = parseInt(prompt("How many characters?"))
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert("Please choose a number between 8 and 128.")
+  }
+  else {
+    var uppercaseletter = confirm("Do you want uppercase letters in your password?")
+    var lowercaseletter = confirm("Do you want lowercase letters in your password?")
+    var numbers = confirm("Do you want numbers in your password?")
+    var specialCharacter = confirm("Do you want special characters in your password?")
+  }
 
-  passwordText.value = password;
 
 }
 
